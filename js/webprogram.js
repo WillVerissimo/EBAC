5693ce72f861cef98cd6d71934d697a605cbb520
WillVerissimo <dev.willv@gmail.com>
2024-05-28 14:56:59 -0300

    Adding Javascript study codes

---
+++ js/webprogram.js
@@ -0,0 +1,20 @@
+document.getElementById("texto").innerHTML= "Meu primeiro texto <b>JS</b>! Feito com document.getElementById().innerHTML";
+alert("Olá! Isso é um alerta feito em JS");
+console.log("Isso é um console.log feito no JS");
+let a,b,c,d; a=5; b=3; c="texto"; d=a+b;
+function minhafuncao(){
+    document.write("Essa é uma função")
+}
+console.log(d);
+let pessoa = "Willian"
+let PESSOA = "WILLIAN"
+console.log(pessoa);
+console.log(PESSOA);
+let segundoNome = "Verissimo";
+console.log(segundoNome)
+//Inserindo um comentário em uma linha
+/*
+Autor: Willian
+Script feito em 28/05/2024
+Este é um comentário de várias linhas 
+*/
