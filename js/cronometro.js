7985d45d888fdd24fb92837b281c7a51a3cc10fe
WillVerissimo <dev.willv@gmail.com>
2024-06-11 19:06:37 -0300

    Adding a timer program

---
+++ js/cronometro.js
@@ -0,0 +1,20 @@
+function comecarContagem() {
+    soma = document.getElementById("contagem").innerHTML;
+    document.getElementById("texto").innerHTML = "Início da contagem"; 
+    document.getElementById("ativar").disabled = true;   
+    tempo = setInterval(function () {
+        document.getElementById("contagem").innerHTML = parseInt(soma) + 1;
+        soma++
+    }, 1000);
+/*    tempo = setTimeout(function () {
+        document.getElementById("texto").innerHTML = "Fim da contagem";
+        }, 5000); // Este código retorna o valor após um período fixo de tempo, definido em milisegundos */
+}
+
+function pararContagem() {
+    //clearTimeout(tempo);
+    document.getElementById("texto").innerHTML = "Fim da contagem";
+    clearInterval(tempo);
+    document.getElementById("ativar").disabled = false;
+    document.getElementById("contagem").innerHTML = 0;
+}
